d162651fbe9d3f0b1288f40f2587b91a7c2f6aba
Sanya Nadeem <sanya.nadeem@gmail.com>
2021-11-03 17:06:24 -0400

    completed web-dev course through lesson 246

---
+++ WeatherProject/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html");
+  });
+
+app.post("/", function(req,res){
+  const query = req.body.cityName;
+  const apiKey = "6b04d7555a8248772f41ecec14cc5152";
+  const unit = "imperial";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query +"&appid=" + apiKey + "&units=" + unit;
+  console.log(url);
+
+  https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write("<p>The weather is currently " + weatherDescription + "<p>");
+      res.write("<h1>The temperature in " + query + " is" + " " + temp + " degrees Fahrenheit.</h1>" );
+      res.write("<img src = " + imageURL +">");
+      res.send();
+    })
+
+  })
+
+})
+
+
+
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
+})
